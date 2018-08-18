d7518dc022e112aae9ba820c89f451a4fd5f124d
Amr M. Kayid <amrkayid2027@gmail.com>
2018-08-18 02:03:31 +0200

    Add train func to train a neural net

---
+++ kaynet/train.py
@@ -0,0 +1,27 @@
+"""
+function that can train a neural net
+"""
+
+from kaynet.tensor import Tensor
+from kaynet.nn import NeuralNet
+from kaynet.loss import Loss, MSE
+from kaynet.optim import Optimizer, SGD
+from kaynet.data import DataIterator, BatchIterator
+
+
+def train(net: NeuralNet,
+          inputs: Tensor,
+          targets: Tensor,
+          num_epochs: int = 5000,
+          iterator: DataIterator = BatchIterator(),
+          loss: Loss = MSE(),
+          optimizer: Optimizer = SGD()) -> None:
+    for epoch in range(num_epochs):
+        epoch_loss = 0.0
+        for batch in iterator(inputs, targets):
+            predicted = net.forward(batch.inputs)
+            epoch_loss += loss.loss(predicted, batch.targets)
+            grad = loss.grad(predicted, batch.targets)
+            net.backward(grad)
+            optimizer.step(net)
+        print(epoch, epoch_loss)
